175e02ad98eeb492a35778aaabdf10d0d0d5daf4
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2026-01-27 23:07:49 +0100

    Update NotifyMenu and add phosphor-icons

--- client/src/components/Layout/RootLayout/components/TopNavMenu/components/NotifyMenu/NotifyMenu.tsx
+++ client/src/components/Layout/RootLayout/components/TopNavMenu/components/NotifyMenu/NotifyMenu.tsx
@@ -1,15 +1,24 @@
 import type React from "react";
 import { motion } from "motion/react"
+import { CaretDoubleRight } from "@phosphor-icons/react"
  
 export const NotifyMenu: React.FC<{isOpen: boolean}> = ({ isOpen }) => {
     return (
         <motion.section
             className="NotifyMenu"
             initial={{ left: '100%' }}
-            animate={{ left: isOpen ? '0%' : '100%' }}
+            animate={{ left: isOpen ? '50%' : '100%' }}
             transition={{ duration: 0.3 }}
         >
-
+            <section className="NotifyMenu_header">
+                <h1>Уведомления</h1>
+                <button>
+                    <CaretDoubleRight size={24} weight="bold" />
+                </button>
+            </section>
+            <section className="NotifyMenu_conetent">
+                
+            </section>
         </motion.section>
     )
 }
---
+++ client/src/components/Layout/RootLayout/components/TopNavMenu/components/NotifyMenu/components/NotifyMenuMessage/NotifyMenuMessage.tsx
@@ -0,0 +1,10 @@
+import type React from "react";
+
+//Test Any
+export const NotifyMenuMessage: React.FC<{data: any}> = ({data}) => {
+    return (
+        <section className="NotifyMenuMessage">
+
+        </section>
+    )
+}
