87610cdadb90149796c22c5f22ef54bc01901ac4
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2026-01-31 20:44:48 +0100

    Add UserList, Update HomePageUserList and Update Path scss styles
    HomePageUserList add fake data

--- client/src/components/Main/HomePage/components/HomePageUserList/HomePageUserList.tsx
+++ client/src/components/Main/HomePage/components/HomePageUserList/HomePageUserList.tsx
@@ -1,4 +1,6 @@
 import { Plus, MagnifyingGlass } from "@phosphor-icons/react"
+import { UserList } from "./components/UserList"
+import avatarImg from "@assets/img/avatar.png"
 
 export const HomePageUserList: React.FC = () => {
     return (
@@ -20,7 +22,42 @@ export const HomePageUserList: React.FC = () => {
                     <input id="search_input" placeholder="Поиск людей и каналов..." />
                 </label>
                 <section className="HomePageUserList_conetent_list_user">
-
+                    <UserList
+                        first_name="Иван"
+                        last_name="Иванов"
+                        avatar={avatarImg}
+                        last_message="Привет! Как дела?"
+                    />
+                    <UserList
+                        first_name="Мария"
+                        last_name="Петрова"
+                        avatar={avatarImg}
+                        last_message="Не забудь про встречу завтра."
+                    />
+                    <UserList
+                        first_name="Алексей"
+                        last_name="Сидоров"
+                        avatar={avatarImg}
+                        last_message="Отправил тебе документы на почту."
+                    />
+                    <UserList
+                        first_name="Иван"
+                        last_name="Иванов"
+                        avatar={avatarImg}
+                        last_message="Привет! Как дела?"
+                    />
+                    <UserList
+                        first_name="Мария"
+                        last_name="Петрова"
+                        avatar={avatarImg}
+                        last_message="Не забудь про встречу завтра."
+                    />
+                    <UserList
+                        first_name="Алексей"
+                        last_name="Сидоров"
+                        avatar={avatarImg}
+                        last_message="Отправил тебе документы на почту."
+                    />
                 </section>
             </section>
         </section>
