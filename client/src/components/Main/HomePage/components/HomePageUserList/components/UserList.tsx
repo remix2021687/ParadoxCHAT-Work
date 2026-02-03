9b5e08db8f3a8e9666b1bc9f24df267e36deba35
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2026-02-03 20:21:55 +0100

    Update UserList and HomePageUserList
    And fix bug with scss styles and css styles
    Delete bad data on Fake Data on components

--- client/src/components/Main/HomePage/components/HomePageUserList/components/UserList.tsx
+++ client/src/components/Main/HomePage/components/HomePageUserList/components/UserList.tsx
@@ -3,14 +3,22 @@ interface UserListProps {
     last_name: string;
     avatar: string;
     last_message: string;
+    last_message_time: string;
+    is_online: boolean;
 }
 
-export const UserList: React.FC<UserListProps> = ({ first_name, last_name, avatar, last_message }) => {
+export const UserList: React.FC<UserListProps> = ({ first_name, last_name, avatar, last_message, last_message_time,is_online }) => {
     return (
         <section className="UserList">
-            <img src={avatar} alt={`${first_name} ${last_name}`} />
+            <section className="UserList_avatar">
+                <img src={avatar} alt={`${first_name} ${last_name}`} />
+                {is_online && <span></span>}
+            </section>
             <section className="UserList_content">
-                <h3>{first_name} {last_name}</h3>
+                <section className="UserList_content_header">
+                    <h3>{first_name} {last_name}</h3>
+                    <h5>{last_message_time}</h5>
+                </section>
                 <p>{last_message}</p>
             </section>
         </section>
