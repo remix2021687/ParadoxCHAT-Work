33dda9dfa43256cd8f679f10057da2ec66212419
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2026-02-07 19:17:31 +0100

    add posts model

---
+++ server/posts/models.py
@@ -0,0 +1,15 @@
+import uuid
+from django.db import models
+from users.models import CustomUser
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255, blank=False, null=False)
+    content = models.TextField(blank=False, null=False, max_length=3000)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="posts")
+    likes = models.ManyToManyField(CustomUser, related_name="liked_posts")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title} | {self.user.username}'
