f9e933f4121cd8e9267d035fb7af7b78707bb13f
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2026-02-22 17:52:21 +0100

    small updat

--- server/api/users/resourceview.py
+++ server/api/users/resourceview.py
@@ -5,9 +5,9 @@ from .serializer import ProfileSerializer
 from users.permissions import IsOwner
 
 class UsersViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated, IsOwner]
-    queryset = CustomUser.objects.all()
 
     def get_queryset(self):
         return Profile.objects.filter(user=self.request.user)
--- server/api/users/serializer.py
+++ server/api/users/serializer.py
@@ -5,7 +5,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ('id', 'first_name', 'last_name', 'username', 'email')
+        fields = ('id', 'first_name', 'last_name', 'username', 'email', 'is_staff', 'is_verified')
 
 
 class ProfileSerializer(serializers.ModelSerializer):
