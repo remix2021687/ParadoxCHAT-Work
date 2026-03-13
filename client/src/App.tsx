88fdff1174e32289f0e769e1fbe7cc4ebc42f962
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2026-03-13 19:06:25 +0100

    Add Redux RTQ
    Add Login and Register request

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,29 @@
 import type React from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router";
+import { ToastContainer } from "react-toastify";
 
 interface AppProps {
-    Router: React.ComponentType<{ components: Record<string, React.ComponentType> }>,
-    Components: Record<string, React.ComponentType<any>>
+	Router: React.ComponentType<{
+		components: Record<string, React.ComponentType>;
+	}>;
+	Components: Record<string, React.ComponentType<any>>;
 }
 
 export const App: React.FC<AppProps> = ({ Router, Components }) => {
-    const navigate = useNavigate()
+	const navigate = useNavigate();
+	const token = localStorage.getItem("token");
 
-    if (!localStorage.getItem("token")) {
-        navigate("auth/")
-    }
-    
-    return (
-        <Router components={Components} />
-    )
-}
+	useEffect(() => {
+		if (!token) {
+			navigate("auth/");
+		}
+	}, [token, navigate]);
+
+	return (
+		<>
+			<ToastContainer />
+			<Router components={Components} />
+		</>
+	);
+};
