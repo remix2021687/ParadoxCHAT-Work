0ed2c9d10d0cb6efeebea70cbddade59c9be8bc0
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2026-03-25 22:11:05 +0100

    Add TrendTopic and Update HomePage

--- client/src/components/Main/HomePage/HomePage.tsx
+++ client/src/components/Main/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { NavigationMenu } from "./components/NavigationMenu/NavigationMenu";
 import { PostMenu } from "./components/PostMenu/PostMenu";
 import { RecomendationComponents } from "./components/RecomendationComponents/RecomendationComponents";
+import { TrendTopic } from "./components/RecomendationComponents/components/TrendTopic/TrendTopic";
 
 export const HomePage: React.FC = () => {
 	useEffect(() => {
@@ -14,7 +15,10 @@ export const HomePage: React.FC = () => {
 			<section className="HomePage_content">
 				<PostMenu />
 			</section>
-			<RecomendationComponents />
+			<section className="HomePage_left">
+				<RecomendationComponents />
+				<TrendTopic />
+			</section>
 		</section>
 	);
 };
